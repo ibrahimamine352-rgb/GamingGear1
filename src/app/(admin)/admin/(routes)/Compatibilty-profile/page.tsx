7e0fb267de5012bf05732c3dada6d3962df45f41
IBRAHIM <ibrahimamine352@gmail.com>
2025-10-28 16:59:55 +0000

    Apply latest UI fixes and PopFormModal type fix

--- src/app/(admin)/admin/(routes)/Compatibilty-profile/page.tsx
+++ src/app/(admin)/admin/(routes)/Compatibilty-profile/page.tsx
@@ -37,6 +37,9 @@ console.log(products)
     price:"0"
     , isFeatured:false,
     createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+    comingSoon: false,
+  outOfStock: false,
+    
   }));
 
   return (
